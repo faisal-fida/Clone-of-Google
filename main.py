3336db46bb2b207bb954f56fd3e5935e69cd0559
Faisal Fida <69955157+fasih4444@users.noreply.github.com>
2022-01-16 19:04:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask('app')
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+app.run(host='0.0.0.0', port=8080)
+# Simple Flask project ...
+# Im still working On the back-end tell me what do you think ! 
